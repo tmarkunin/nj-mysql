9a6098825d2c521da81fa7c527721d279d62186e
tmarkunin <tmarkunin@gmail.com>
2019-06-05 09:52:00 +0300

    Update helpers.sh

--- helpers.sh
+++ helpers.sh
@@ -1,2 +1,5 @@
 #Create firewall rule for GCP port 3000
 gcloud compute firewall-rules create nodejs-default --allow tcp:3000
+#For docker-compose execution with docker server in GCP
+docker-machine create --driver google --google-project docker-242218 --google-machine-image https://www.googleapis.com/compute/v1/projects/ubuntu-os-cloud/global/images/family/ubuntu-1604-lts  --google-machine-type n1-standard-1  --google-zone europe-west1-b   ghost 
+eval $(docker-machine env ghost)
