c74b39fd75b17122795372869a1ad3d3b330b7cd
timur <timur_markunin@epam.com>
2019-06-26 15:13:31 +0300

    aks helper added

--- helpers_AKS.sh
+++ helpers_AKS.sh
@@ -4,6 +4,21 @@
 az login
 
 az group create --name aksresgrp --location eastus
+#create container registry
+az acr create \
+    --name tmaregistry \
+    --resource-group aksresgrp \
+    --sku Basic 
+
+#take note of loginServer in returned JSON
+sudo az acr login --name tmaregistry
+
+sudo docker tag tmarkunin/prometheus:latest tmaregistry.azurecr.io/prometheus:latest
+
+sudo docker push tmaregistry.azurecr.io/prometheus:latest
+
+# Start Azure Pipelines https://azure.microsoft.com/en-us/services/devops/pipelines/
+
 
 # create AKS cluster (2 nodes is the limit for the trial account)
 az aks create \
