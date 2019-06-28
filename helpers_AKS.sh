91621b8c080dcf2d4ac531edd4f522e994e10f7e
timur <timur_markunin@epam.com>
2019-06-28 10:25:41 +0300

    fix helper

--- helpers_AKS.sh
+++ helpers_AKS.sh
@@ -17,12 +17,29 @@ sudo docker tag tmarkunin/prometheus:latest tmaregistry.azurecr.io/prometheus:la
 
 sudo docker push tmaregistry.azurecr.io/prometheus:latest
 
+az acr update -n tmaregistry --admin-enabled true
+
+az acr credential show --name tmaregistry
+#put password into DevOps pipeline variavles: Press Edit for selected build pipeline and then press 3 dots button right from RUN button
+# You can add Service Connections (ACR,AKS) through DevOps project settings (Low left corner of UI)
 # Start Azure Pipelines https://azure.microsoft.com/en-us/services/devops/pipelines/
+
+#Task based build pipeline can look like
+steps:
+- task: Docker@2
+  displayName: Build and push docker image
+  inputs:
+    containerRegistry: 'tma-acr-connection'
+    repository: 'dotnetcore'
+    command: 'buildAndPush'
+    Dockerfile: '**/Dockerfile'
 
 
 # create AKS cluster (2 nodes is the limit for the trial account)
 az aks create \
     --resource-group aksresgrp \
     --name akscluster \
-    --node-count 2 \ 
+    --node-count 2 \
     --generate-ssh-keys
+
+
