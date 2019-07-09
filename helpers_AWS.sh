0c4b684124f89d5db1975e385356834cd5443a22
timur <timur_markunin@epam.com>
2019-07-09 16:51:51 +0300

    added CodeBuild spec

---
+++ helpers_AWS.sh
@@ -0,0 +1,4 @@
+#Create ECR registry and code build service role to be able to push images
+https://aws.amazon.com/blogs/devops/build-a-continuous-delivery-pipeline-for-your-container-images-with-amazon-ecr-as-source/
+
+#Create new build in CodeBuild and point it to github repo
